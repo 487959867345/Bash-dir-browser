da6184395d61e57834214e25e74c34f013f2e43d
CatchyAintIt <57611181+487959867345@users.noreply.github.com>
2020-07-15 20:51:35 +0300

    Add files via upload

---
+++ test 2.bash
@@ -0,0 +1,27 @@
+
+echo -n "Start Dir Browser y/n?"
+read startVar
+
+while [ $tartVar = "y" ]
+do
+	echo "what dir do you want to see listed"
+	echo -n
+	read name
+	clear
+
+	echo "#############################################################"
+	echo "Current Directoy: $name"
+	ls $name
+	echo "#############################################################"
+
+	echo -n “Open new Dir  List y/n?”
+	read answer
+	if [ $answer = "y" ] ; then
+		clear
+	else
+		clear
+		echo "Thank you for using iamseahawks Dir Browser!"
+		echo "You support at "
+		break;
+	fi
+done
